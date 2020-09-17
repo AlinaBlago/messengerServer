d67544dca945ae47d972b831bf637a8d576559e8
Alina <alinablago19@gmail.com>
2020-09-17 16:20:37 +0300

    Fixed class AuthorizationResponse, added HttpResponses

--- src/main/java/com/finalproject/server/serverResponse/AuthorizationResponse.java
+++ src/main/java/com/finalproject/server/serverResponse/AuthorizationResponse.java
@@ -1,27 +1,48 @@
 package com.finalproject.server.serverResponse;
 
+import java.net.http.HttpResponse;
+
 public class AuthorizationResponse {
-    private int responseID;
-    // 0 - все ок
-    // 1 - ошибка неизвестная
-    // 2 - такой пользователь уже есть
-    // 3 - неверный логин или парль
-    private String responseMessage;
+    private HttpResponse<String> response;
+    private HttpResponse<Integer> responseStatusCode;
 
-    public int getResponseID() {
-        return responseID;
+    public String getResponse() {
+        return response.body();
     }
 
-    public void setResponseID(int responseID) {
-        if(responseID > 3 || responseID < 0) return;
-        this.responseID = responseID;
+    public void setResponse(HttpResponse<String> response) {
+        this.response = response;
     }
 
-    public String getResponseMessage() {
-        return responseMessage;
+    public int getResponseStatusCode() {
+        return responseStatusCode.statusCode();
     }
 
-    public void setResponseMessage(String responseMessage) {
-        this.responseMessage = responseMessage;
+    public void setResponseStatusCode(HttpResponse<Integer> responseStatusCode) {
+        this.responseStatusCode = responseStatusCode;
     }
+
+    //    private int responseID;
+//    // 0 - все ок
+//    // 1 - ошибка неизвестная
+//    // 2 - такой пользователь уже есть
+//    // 3 - неверный логин или парль
+//    private String responseMessage;
+//
+//    public int getResponseID() {
+//        return responseID;
+//    }
+//
+//    public void setResponseID(int responseID) {
+//        if(responseID > 3 || responseID < 0) return;
+//        this.responseID = responseID;
+//    }
+//
+//    public String getResponseMessage() {
+//        return responseMessage;
+//    }
+//
+//    public void setResponseMessage(String responseMessage) {
+//        this.responseMessage = responseMessage;
+//    }
 }
