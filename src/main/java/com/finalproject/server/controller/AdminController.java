7c99ef2593023f4319d7bce9482d0599d1f7c7a4
Alina <alinablago19@gmail.com>
2020-09-17 23:58:44 +0300

    Added new method (loadUsersForAdmin) in AdminController

--- src/main/java/com/finalproject/server/controller/AdminController.java
+++ src/main/java/com/finalproject/server/controller/AdminController.java
@@ -1,7 +1,35 @@
 package com.finalproject.server.controller;
 
+import com.finalproject.server.service.impl.MessageServiceImpl;
+import com.finalproject.server.service.impl.UserServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManager;
+import java.util.Collections;
+import java.util.Set;
 
 @RestController
 public class AdminController {
+    MessageServiceImpl messageService = new MessageServiceImpl();
+    UserServiceImpl userService = new UserServiceImpl();
+
+    @RequestMapping(value = "/LoadUsersForAdmin" , method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity.BodyBuilder LoadUsersForAdmin(Long id, String key){
+
+        Set<String> users = Collections.singleton(messageService.getUserChats(id).toString());
+
+        userService.findAll().forEach(item -> {
+            users.add(item.getLogin());
+        });
+
+        return ResponseEntity.status(HttpStatus.OK);
+    }
+
+
 }
